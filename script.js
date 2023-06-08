bc6204b7af93b57f72748271bead23dc58f44f9a
FedericoRosso97 <federicorosso97@gmail.com>
2023-06-08 09:23:09 +0200

    es. hello vue

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('ciao');
+const {createApp}=Vue;
+
+createApp({
+    data(){
+        
+          return{
+            text:'hello from Federico!',
+          }
+    }
+
+}).mount('#app');
